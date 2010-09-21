af9ffc5dcd9855bed206dfee5d38698a3090b31f
Gerhard Lazu <gerhard@lazu.co.uk>
2010-09-21 16:10:44 +0100

    System-wide RVM setup First stab

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+maintainer        "Paper Cavalier"
+maintainer_email  "code@papercavalier.com"
+license           "Apache 2.0"
+description       "Installs and configures rvm system wide"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.textile'))
+version           "0.1.0"
+
+recipe "rvm", "Installs rvm system-wide"
+
+depends "git"
