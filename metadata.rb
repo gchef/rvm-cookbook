9b61d02e826c9d041251ecd4dbe44dec118f79eb
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-01-24 01:28:55 +0000

    Remove version for now. It's not like we're maintaining this on the Opscode site.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer_email  "code@papercavalier.com"
 license           "Apache 2.0"
 description       "Installs RVM system-wide"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.2"
 
 recipe "rvm", "Installs RVM system-wide"
 
