a072edcaf8da65145490ba05e4314cfd53a83d25
Code Monkey Max <code@papercavalier.com>
2011-04-20 16:40:30 +0200

    Keeping up with wayneeseguine's changes

--- definitions/install_rvm.rb
+++ definitions/install_rvm.rb
@@ -1,7 +1,7 @@
 define :install_rvm do
   bash "install_rvm" do
     code <<-EOH
-    bash < <(curl -B http://rvm.beginrescueend.com/install/rvm)
+    bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
     EOH
     not_if { File.exists? "/usr/local/rvm" }
   end
