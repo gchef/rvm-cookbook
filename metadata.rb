d9242cb6e2a5ea73b29eaee3d6f86bba80f68f8e
Gerhard Lazu <gerhard@lazu.co.uk>
2011-09-12 23:48:21 +0100

    Depends on bootstrap
    Has 2 recipes

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,9 @@ maintainer_email  "gerhard@lazu.co.uk"
 license           "Apache 2.0"
 description       "Installs RVM as root, shared across all users"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.0"
+version           "1.0.1"
+
+depends "bootstrap"
 
 recipe "rvm", "Installs RVM as root, shared across all users"
+recipe "rvm::users", "Sets up rvm for specific users"
