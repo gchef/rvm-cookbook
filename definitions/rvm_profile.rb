547913f9e8c1f688fd6c7b936bcedb42e677d24e
Gerhard Lazu <gerhard@lazu.co.uk>
2011-10-05 15:10:30 +0100

    Wasn't taking into account root's home path

--- definitions/rvm_profile.rb
+++ definitions/rvm_profile.rb
@@ -1,10 +1,11 @@
 define :rvm_profile do
   username = params[:name]
+  user_home = (username == "root") ? "/root" : "/home/#{username}"
 
   bash "Add rvm sourcing to #{username} bash profile" do
     code %{
-      echo "source '#{node[:rvm_script]}'" >> /home/#{username}/.profile
+      echo "source '#{node[:rvm_script]}'" >> #{user_home}/.profile
     }
-    only_if "[ $(grep -c rvm /home/#{username}/.profile) -eq 0 ]"
+    only_if "[ $(grep -c rvm #{user_home}/.profile) -eq 0 ]"
   end
 end
